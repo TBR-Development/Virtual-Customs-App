a82977e19ecbf3b957d625291f415a5e076d395d
Nimbi <JoshuaVL1988@protonmail.com>
2023-12-20 13:48:59 -0700

    :star2: Create Virtual Customs App

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Virtual-Customs-App
-Electron menu app for Virtual Customs
+<h1 align="center">Virtual Customs App</h1>
+
+<p align="center">
+<img width=300" alt="Icon.png" src="/assets/Icon.png"><br />
+</p>
+
+[Electron][Electron] menubar application that wraps Virtual Customs.
+
+---
+
+**Requirements**
+
+- [NodeJS][NodeJS]
+
+---
+
+**Instructions:**
+
+- Run the following commands in your terminal:
+
+```sh
+npm i -g yarn && npm i git
+git clone https://github.com/tbr-development/Virtual-Customs-App && cd Virtual-Customs-App
+yarn install
+yarn package
+```
+
+- Check the `out` folder for the packaged application to run.
+
+---
+
+**Notes**
+
+- If you want to package a squirrel based app/installer, you can use `yarn make`. Once done check the `out/make` folder for your new squirrel based app/installer.
+- Windows and linux users can use `Control+X` or `Command+X` to quit the app.
+MacOS users can use `CMD+Q` to quit the app.
+- It is recommended to have a systemwide ad blocker when using this app to get rid of excess ads. AdGuard Desktop has been tested to work perfectly with electron webapps.
+
+
+[Electron]: https://www.electronjs.org/
+[NodeJS]: https://nodejs.org
---
+++ main.js
@@ -0,0 +1,50 @@
+// Modules to control application life and create native browser window
+const { app, globalShortcut } = require("electron");
+const path = require("path");
+const { menubar } = require("menubar");
+
+const mb = menubar({
+  browserWindow: { width: 1250, height: 650 },
+  preloadWindow: true,
+  icon: path.join(__dirname, "/MenuIcon.png"),
+  webPreferences: {
+    partition: "persist:virtualcustoms",
+  },
+});
+
+mb.app.commandLine.appendSwitch(
+  "disable-backgrounding-occluded-windows",
+  "true"
+);
+
+mb.on("ready", () => {
+  console.log("app is ready");
+
+  win = mb.window;
+  // win.openDevTools();
+
+  // First URL
+  win.loadURL("https://virtualcustoms.net/");
+
+  // mb.on('after-create-window', () => {};
+
+  // Once dom-ready
+  // win.webContents.once("dom-ready", () => {});
+
+  globalShortcut.register("CommandOrControl+X", () => {
+    if (process.platform !== "darwin") {
+      app.removeAllListeners();
+      app.quit();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.removeAllListeners();
+    app.quit();
+  }
+});
